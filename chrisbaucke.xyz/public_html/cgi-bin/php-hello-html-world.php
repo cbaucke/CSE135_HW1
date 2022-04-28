914e68a606267a05872df8820ca12138bc30ad78
Chris Baucke <cbaucke@ucsd.edu>
2022-04-27 20:36:50 -0700

    Testing first JSP file

--- chrisbaucke.xyz/public_html/cgi-bin/php-hello-html-world.php
+++ chrisbaucke.xyz/public_html/cgi-bin/php-hello-html-world.php
@@ -5,7 +5,7 @@
         <title>Hello, PHP!</title>
     </head>
     <body>
-        <?php echo "<h1 align:center>Chris was Here - Hello, PHP!</h1>" . "<br>"; ?>
+        <?php echo "<h1 align:center>Hello, PHP!</h1>" . "<br>"; ?>
         <?php echo "Current Time: " . date("l M d h:i:s A Y") . "<br>"; ?>
         <?php echo "Your IP Address: " . $_SERVER["REMOTE_ADDR"] . "<br>"; ?>
     </body>
--- chrisbaucke.xyz/public_html/cgi-bin/php-hello-json-world.php
+++ chrisbaucke.xyz/public_html/cgi-bin/php-hello-json-world.php
@@ -1,5 +1,5 @@
 <?php header("Content-type: application/json"); ?>
 <?php
-    $packet = array("title"=>"Hello, PHP!", "heading"=>"Chris was Here - Hello, PHP!", "time"=>date("l M d h:i:s A Y"), "IP"=>$_SERVER["REMOTE_ADDR"]);
+    $packet = array("title"=>"Hello, PHP!", "heading"=>"Hello, PHP!", "time"=>date("l M d h:i:s A Y"), "IP"=>$_SERVER["REMOTE_ADDR"]);
     echo json_encode($packet);
 ?>
