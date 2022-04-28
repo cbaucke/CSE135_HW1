7dceb0f13b5caf42e75baaa4cc8d0bcd8c2093dd
Chris Baucke <cbaucke@ucsd.edu>
2022-04-27 23:46:39 -0700

    Added links, fixed typo

--- chrisbaucke.xyz/public_html/cgi-bin/ruby-environment.rb
+++ chrisbaucke.xyz/public_html/cgi-bin/ruby-environment.rb
@@ -2,7 +2,7 @@
 require 'cgi'
 
 cgi = CGI.new("html4")
-html = "<html><head><title>Environment Variables</title></head><body><h1 align="center">Environment Variables</h1>"
+html = "<html><head><title>Environment Variables</title></head><body><h1 align='center'>Environment Variables</h1>"
 ENV.each_pair do |key, value|
     html += "<p>#{key}=#{value}</p>"
 end
