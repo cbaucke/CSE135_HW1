cc7278f2c3e55da897ae8a92f90763a7c1ddf064
Chris Baucke <cbaucke@ucsd.edu>
2022-04-27 23:49:00 -0700

    Fixed ruby echo messages

--- chrisbaucke.xyz/public_html/cgi-bin/ruby-general-request-echo.rb
+++ chrisbaucke.xyz/public_html/cgi-bin/ruby-general-request-echo.rb
@@ -8,9 +8,9 @@ cgi.out{
     cgi.head{ "\n" + cgi.title{"General Request Echo"}} + 
     cgi.body{ "\n" +
         cgi.h1{"General Request Echo"} + "\n" + 
-        cgi.p{"HTTP Protocol: " + cgi["server_protocol"]} + "\n" + 
-        cgi.p{"HTTP Method: " + cgi["request_method"]} + "\n" + 
-        cgi.p{"Query String: " +cgi["query_string"]} + "\n" + 
+        cgi.p{"HTTP Protocol: " + cgi.server_protocol} + "\n" + 
+        cgi.p{"HTTP Method: " + cgi.request_method} + "\n" + 
+        cgi.p{"Query String: " +cgi.query_string} + "\n" + 
         cgi.p{"Message Body: " + requestBody}
     }
 }
--- chrisbaucke.xyz/public_html/cgi-bin/ruby-get-echo.rb
+++ chrisbaucke.xyz/public_html/cgi-bin/ruby-get-echo.rb
@@ -6,6 +6,6 @@ cgi.out{
     cgi.head{ "\n" + cgi.title{"GET Request Echo"}} + 
     cgi.body{ "\n" +
         cgi.h1{"GET Request Echo"} + "\n" + 
-        cgi.p{"Query String: " + cgi["query_string"]}
+        cgi.p{"Query String: " + cgi.query_string}
     }
 }
