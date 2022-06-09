7a065d2b7235236c27f9cd2bb94eb2de678a7fdf
Chris Baucke <cbaucke@ucsd.edu>
2022-06-08 21:39:42 -0700

    testing

--- reporting.chrisbaucke.xyz/public_html/hash.php
+++ reporting.chrisbaucke.xyz/public_html/hash.php
@@ -1,6 +1,5 @@
 <?php 
     require_once("./vendor/autoload.php");
-    echo $_POST;
     if(isset($_POST["password"]) && $_POST["password"] != ""){
         $hash = password_hash($_POST["password"], PASSWORD_DEFAULT, []);
         $_POST["password"] = $hash;
@@ -8,6 +7,6 @@
         $db = $m->users;
         $col = $db->users;
         $document = $col->insertOne($_POST);
+        echo $document["_id"];
     }
-    file_put_contents("testOutput.txt", "Worked!");
 ?>
